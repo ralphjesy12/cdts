5e273c82a675c17626e3f23cc6d858e014b798a9
dondonherrera <dondonherrera0402@yahoo.com>
2015-08-14 20:38:53 +0800

    Registration Link and Remember Me
    Comment the Registration Link  and Remember me at the Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,7 @@
 		<img src="{{ asset('img/logo.png') }}">
 		<div class="logo">Crew Development Training System</div>
 		<!-- Main Form -->
+		<br>
 		<div class="login-form-1">
 			<form id="login-form" class="text-left" method="POST" action="/login">
 				{!! csrf_field() !!}
@@ -35,9 +36,9 @@
 							<input type="password" class="form-control" id="lg_password" name="password" placeholder="password">
 						</div>
 						<div class="form-group login-group-checkbox">
-							<input type="checkbox" id="lg_remember" name="remember">
-							<label for="lg_remember">remember</label>
-						</div>
+							<!--<input type="checkbox" id="lg_remember" name="remember">
+							<label for="lg_remember">remember</label> -->
+						</div>   
 					</div>
 					<button type="submit" class="login-button">
 						<i>GO</i>
@@ -45,9 +46,9 @@
 					</button>
 				</div>
 				<div class="etc-login-form">
-					<p>forgot your password? <a href="#">click here</a></p>
-					<p>new user? <a href="/register">create new account</a></p>
-				</div>
+					<!--<p>forgot your password? <a href="#">click here</a></p>
+					<p>new user? <a href="/register">create new account</a></p>-->
+				</div>  
 			</form>
 		</div>
 		<!-- end:Main Form -->
