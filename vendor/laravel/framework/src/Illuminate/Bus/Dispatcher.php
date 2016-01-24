0791447047f6c9e86da526c790d982e6f089819a
Ralph John Galindo <ralphjesy@gmail.com>
2016-01-24 22:26:48 +0800

    Revert "Dispatcher Update"
    This reverts commit 161c8223b6c7b5b72a6737e8d17c4a6a7d4a0b76.

--- vendor/laravel/framework/src/Illuminate/Bus/Dispatcher.php
+++ vendor/laravel/framework/src/Illuminate/Bus/Dispatcher.php
@@ -137,7 +137,7 @@ class Dispatcher implements DispatcherContract, QueueingDispatcher, HandlerResol
     }
 
     /**
-     * Get a parameter value for a marshalled command.
+     * Get a parameter value for a marshaled command.
      *
      * @param  string  $command
      * @param  \ArrayAccess  $source
@@ -145,7 +145,8 @@ class Dispatcher implements DispatcherContract, QueueingDispatcher, HandlerResol
      * @param  array  $extras
      * @return mixed
      */
-    protected function getParameterValueForCommand($command, ArrayAccess $source, ReflectionParameter $parameter, array $extras = [])
+    protected function getParameterValueForCommand($command, ArrayAccess $source,
+        ReflectionParameter $parameter, array $extras = [])
     {
         if (array_key_exists($parameter->name, $extras)) {
             return $extras[$parameter->name];
