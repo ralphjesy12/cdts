bbe1746a01da69e4ccac1d660a1c7636aec68275
Ralph John Galindo <ralphjesy@gmail.com>
2016-02-08 12:55:28 +0800

    Fix SMS Notif
    * Added :  Split messages greater than 160

--- config/chikka.php
+++ config/chikka.php
@@ -6,9 +6,12 @@
 
 return [
     // Shortcode to use
-    'shortcode'=> '292902521',
+    // 'shortcode'=> '292902521',
+    'shortcode'=> '29290819',
     // Client ID
-    'client_id'=> 'c17fb13d19f5ec1cd33e32f2ac7164d95a0667bd7cb7cc4c732782f44c22d8b9',
+    // 'client_id'=> 'c17fb13d19f5ec1cd33e32f2ac7164d95a0667bd7cb7cc4c732782f44c22d8b9',
+    'client_id'=> '8706aa90db58e46db04a1b293f4c75558e716d3abb9c105219eb371a07ce1e9d',
     // Secret key
-    'secret_key'=> '5f1c8d14996487a28bdb13e4c52f6e3b649a0c56d1183767850fa976dacbb050',
+    // 'secret_key'=> '5f1c8d14996487a28bdb13e4c52f6e3b649a0c56d1183767850fa976dacbb050',
+    'secret_key'=> 'a861673b16c5738f2e525836a4a3c9e8695c870d3252818f6da5e119aee24e9d',
 ];
